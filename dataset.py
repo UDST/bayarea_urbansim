835c63a483c47e1e057075fb150245affc4ee2a2
Fletcher Foti <ffoti@berkeley.edu>
2013-09-10 13:26:52 -0700

    initial version of the bay area json models

---
+++ dataset.py
@@ -0,0 +1,199 @@
+import numpy as np, pandas as pd
+import time, os
+from synthicity.utils import misc
+from synthicity.urbansim import dataset, networks
+import warnings
+
+warnings.filterwarnings('ignore',category=pd.io.pytables.PerformanceWarning)
+
+# this is the central location to do all the little data format issues that will be needed by all models
+
+class BayAreaDataset(dataset.Dataset):
+
+  def __init__(self,filename):
+    super(BayAreaDataset,self).__init__(filename)
+
+  def fetch(self,name,addnodeid=0,convertsrid=0,addbuildingid=0,pya=None,direct=0):
+
+    if direct: return self.store[name]
+    if name in self.d: return self.d[name]
+
+    tbl = None
+    if name == 'buildings': tbl = self.fetch_buildings()
+    elif name == 'homesales': tbl = self.fetch_homesales()
+    elif name == 'costar': tbl = self.fetch_costar()
+    elif name == 'households': tbl = self.fetch_households()
+    elif name == 'batshh': tbl = self.fetch_batshh()
+    elif name == 'bats': tbl = self.fetch_bats()
+    elif name == 'nets': tbl = self.fetch_nets()
+    elif name == 'networks': tbl = self.fetch_networks()
+    elif name == 'apartments': tbl = self.fetch_apartments()
+    elif name == 'zoning_for_parcels': tbl = self.fetch_zoning_for_parcels()
+    else: tbl = self.store[name]
+   
+    if addnodeid:
+        assert self.networks 
+        tbl = self.networks.addnodeid(tbl,convertsrid)
+
+    if addbuildingid:
+        assert self.networks
+        tbl = self.networks.addbuildingid(self,tbl)
+
+    self.d[name] = tbl
+    return tbl
+
+  def fetch_costar(self):
+    costar = self.store['costar']
+    costar = costar[costar['averageweightedrent']>0]
+    costar['stories'] = costar['number_of_stories']
+    costar['general_type'] = costar['PropertyType']
+    costar = costar[costar.general_type.isin(["Office","Retail","Industrial","Flex"])]
+    return costar
+
+  def fetch_apartments(self):
+    apartments = self.join_for_field(self.store['apartments'],'parcels','parcel_id','_node_id')
+    apartments = apartments.dropna()
+    apartments['rent'] = (apartments['MinOfLowRent']+apartments['MaxOfHighRent'])/2.0
+    apartments['rent'] /= apartments['AvgOfSquareFeet']
+    apartments['unit_sqft'] = apartments['AvgOfSquareFeet'] 
+    return apartments
+
+  def fetch_buildings(self,add_xy_from_parcels=1):
+    buildings = self.store['buildings']
+
+    buildings['rent'] = np.nan
+
+    buildings['rental'] = buildings['tenure']==1
+
+    for t in ['HS','HT','HM']: buildings['general_type'][buildings['building_type'] == t] = 'Residential'
+    for t in ['RS','RB']: buildings['general_type'][buildings['building_type'] == t] = 'Retail'
+    for t in ['IL','IW','IH']: buildings['general_type'][buildings['building_type'] == t] = 'Industrial'
+    for t in ['OF']: buildings['general_type'][buildings['building_type'] == t] = 'Office'
+    for t in ['HO','SC','MR','ME','MT','GV','RC','PS']: 
+      buildings['general_type'][buildings['building_type'] == t] = 'Flex'
+
+    buildings['residential_units'][buildings['residential_units'] == 0] = np.nan
+    buildings['residential_units'] = buildings['residential_units'].fillna(1) 
+    buildings['residential_units'][buildings['residential_units']>300] = 300
+
+    buildings['stories'][buildings['stories'] == 0] = np.nan
+    buildings['stories'] = buildings['stories'].fillna(1) 
+
+    #buildings['year_built'] = buildings['year_built'][buildings['year_built'] < 1870] = np.nan
+    #buildings['year_built'] = buildings['year_built'][buildings['year_built'] > 2015] = np.nan
+
+    print "WARNING: imputing building square feet where it doesn't exist"
+    buildings['building_sqft'][buildings['building_sqft'] == 0] = np.nan
+    buildings['building_sqft'] = buildings['building_sqft'].fillna(buildings['building_sqft'].mean())
+    buildings['unit_sqft'] = buildings['building_sqft'] / buildings['residential_units']
+    buildings['unit_sqft'][buildings['unit_sqft']>3000] = 3000
+
+    print "WARNING: imputing lot size where it doesn't exist"
+    buildings['lot_size'][buildings['lot_size'] == 0] = np.nan
+    buildings['lot_size'] = buildings['lot_size'].fillna(buildings['lot_size'].mean())
+    buildings['unit_lot_size'] = buildings['lot_size'] / buildings['residential_units']
+
+    if add_xy_from_parcels and 'x' not in buildings.columns: # don't add twice
+        buildings = pd.merge(buildings,self.fetch('parcels',addnodeid=1)[['x','y']],
+                                left_on='parcel_id',right_index=True,how='left')
+
+    return buildings
+
+  def fetch_households(self,tenure=None):
+
+    households = self.store['households']
+    households['income'][households['income'] < 0] = 0
+    households['income_quartile'] = pd.qcut(households['income'],4).labels
+
+    if tenure == "rent": households = households[households['tenure']==1]
+    elif tenure == "sales": households = households[households['tenure']<>1]
+    
+    households['HHINCOME'] = households['income']/10000.0
+
+    return households
+
+  def fetch_homesales(self):
+
+    homesales = self.store['homesales']
+
+    homesales['Sale_price'] = homesales['Sale_price'].str.replace('$','')
+    homesales['Sale_price'] = homesales['Sale_price'].str.replace(',','')
+    homesales['Sale_price_flt'] = homesales['Sale_price'].astype('f4')
+    homesales['Sale_price_flt'] /= homesales['SQft'] 
+    homesales = homesales[homesales.SQft>0]
+    homesales["year_built"] = homesales["Year_built"]
+    homesales["unit_lot_size"] = homesales["Lot_size"]
+    homesales["unit_sqft"] = homesales["SQft"]
+
+    return homesales
+
+  def fetch_nets(self):
+
+    nets = self.store['nets']
+    nets = self.join_for_field(nets,'buildings','building_id','_node_id')
+    return nets
+
+  def fetch_batshh(self,tenure=None):
+
+    batshh = self.store['batshh']
+
+    batshh = batshh[batshh['HHINCOME'] < 16] # remove bogus income records
+    batshh['income_quartile'] = pd.qcut(batshh['HHINCOME'],4).labels
+  
+    if tenure == "rent": batshh = batshh[batshh['TENURE']==1]
+    elif tenure == "sales": batshh = batshh[batshh['TENURE']<>1]
+    
+    return batshh
+
+  def fetch_zoning_for_parcels(self):
+
+    zoning_for_parcels = self.store['zoning_for_parcels']
+
+    zoning_for_parcels['index'] = zoning_for_parcels.index
+    zoning_for_parcels = zoning_for_parcels.drop_duplicates(cols='index')
+    del zoning_for_parcels['index']
+
+    return zoning_for_parcels
+
+  def fetch_bats(self):
+
+    bats = self.store['bats']
+
+    bats = bats[bats['HHINCOME'] < 16] # remove bogus income records
+    bats = bats[bats['HHVEH'] < 10] # remove bogus vehicle records
+    bats = bats[bats['AGE'] < 100] # remove bogus age records
+    bats = bats.dropna(subset=['origpoint','destpoint']) # remove points without origins
+    bats['EMPLOYED'] = bats['EMPLOYED'] == 1
+    bats['FEMALE'] = bats['GENDER'] == 2
+    bats['SINGLEFAMILY'] = bats['DWELLTYP'] == 1
+    bats['OWNER'] = bats['TENURE'] == 2
+    bats['CHILD'] = bats['AGE'] < 16 
+    bats['YOUNGADULT'] = (bats['AGE'] >= 16) * (bats['AGE'] < 28)
+
+    return bats
+
+  def fetch_networks(self,reload=True,maxdistance=30,rootdir=None,custom_impedances=None):
+    if not reload: return networks.Networks(os.path.join(misc.data_dir(),'network%d.pkl'))
+    
+    network = networks.Networks()
+    network.process_network(maxdistance,rootdir,walkminutes=1,custom_impedances=custom_impedances)
+    self.networks = network
+    return self.networks
+
+  # this is a shortcut function to join the table with dataset.fetch(tblname) 
+  # using the foreign_key in order to add fieldname to the source table
+  def join_for_field(self,table,tblname,foreign_key,fieldname):
+    if type(table) == type(''): table = self.fetch(table)
+    if foreign_key == None: # join on index
+        return pd.merge(table,self.fetch(tblname)[[fieldname]],left_index=True,right_index=True)
+    return pd.merge(table,self.fetch(tblname)[[fieldname]],left_on=foreign_key,right_index=True)
+
+  # the norental and noowner leave buildings with unassigned tenure
+  def building_filter(self,norental=0,noowner=0,residential=1,nofilter=0):
+    buildings = self.fetch('buildings')
+    if nofilter: return buildings
+    if residential: buildings = buildings[(buildings['general_type'] == 'Residential')]
+    else:           buildings = buildings[(buildings['general_type'] <> 'Residential')]
+    if norental:    buildings = buildings[buildings['tenure'] <> 1]
+    if noowner:     buildings = buildings[buildings['tenure'] <> 2]
+    return buildings 
---
+++ variables.py
@@ -0,0 +1,161 @@
+import time, numpy as np, pandas as pd
+from synthicity.utils import misc
+
+# computes a weighted rent by non-residential building type and the proportion of each building type
+def compute_nonres_building_proportions(dataset,year):
+
+  buildings = dataset.fetch('buildings')
+  buildings = buildings[buildings['general_type'] <> 'Residential']
+  buildings['rent'] = dataset.load_attr('nonresidential_rent',year)
+
+  tmp = buildings[['_node_id','building_sqft','general_type']]
+  tmp['weightedrent'] = tmp['building_sqft']*buildings['rent']
+
+  totsum = tmp.groupby('_node_id').sum().rename(columns={"building_sqft":"totsum"})
+  totsum["weightedrent"] /= totsum["totsum"]
+  del tmp["weightedrent"]
+
+  offsum = tmp[tmp['general_type']=='Office'].groupby('_node_id').sum().rename(columns={"building_sqft":"offsum"})
+  indsum = tmp[tmp['general_type']=='Industrial'].groupby('_node_id').sum().rename(columns={"building_sqft":"indsum"})
+  retsum = tmp[tmp['general_type']=='Retail'].groupby('_node_id').sum().rename(columns={"building_sqft":"retsum"})
+
+  nodesums = totsum.join(offsum).join(indsum).join(retsum)
+  for fname in ["offsum","retsum","indsum"]: nodesums[fname] = nodesums[fname].fillna(0)
+  for fname in ["off","ret","ind"]: nodesums[fname+"pct"] = nodesums[fname+"sum"]/nodesums["totsum"]
+
+  return nodesums
+
+def compute_households(dataset,year,rent=0,sales=0):
+
+  households = dataset.join_for_field(dataset.fetch('households'),'buildings','building_id','_node_id')
+
+  if rent: households = households[households['tenure']==1]
+  if sales: households = households[households['tenure']<>1]
+
+  nodesums = households[['_node_id','persons']].fillna(1).groupby('_node_id').sum() \
+                                          .rename(columns={'persons':'noderenters'})
+
+  return nodesums
+
+def quantile_list(df, quantiles):
+  return type(df)(dict([(q,df.quantile(q)) for q in quantiles]))
+
+def discretize_population(segment,numsegments=None):
+  width = 1.0/numsegments
+  quantiles = np.arange(numsegments)*width+width/2.0
+  return quantile_list(segment,quantiles).transpose()
+
+def compute_res_building_empty_units(dataset,year,rent=0,sales=0):
+
+  #if rent: assert 0
+  buildings = dataset.building_filter(residential=1,norental=0,noowner=0)
+
+  households = dataset.fetch('households')['building_id'].value_counts()
+
+  if rent: buildings['rent'] = dataset.load_attr('residential_rent',year)
+  else: buildings['rent'] = 0
+  if sales: buildings['sales_price'] = dataset.load_attr('residential_sales_price',year)
+  else: buildings['sales_price'] = 0
+
+  supply = buildings['residential_units'].sub(households,fill_value=0)
+  supply = supply[supply > 0]
+  
+  bids = np.repeat(supply.index,supply.values.astype('int'))
+  buildings = buildings.ix[bids]
+  buildings['residential_units'] = 1
+
+  return buildings
+
+def compute_res_building_averages(dataset,year,rent=0,sales=0):
+
+  buildings = dataset.fetch('buildings')
+  buildings = buildings[buildings['general_type'] == 'Residential']
+
+  if rent: buildings['rent'] = dataset.load_attr('residential_rent',year)
+  else: buildings['rent'] = 0
+  if sales: buildings['sales_price'] = dataset.load_attr('residential_sales_price',year)
+  else: buildings['sales_price'] = 0
+
+  nodeaverages = buildings[['_node_id','unit_sqft','unit_lot_size','rent','sales_price']] \
+                                     .groupby('_node_id').mean()
+  nodesums = buildings[['_node_id','residential_units']].groupby('_node_id').sum()
+
+  return nodeaverages.join(nodesums)
+
+def add_current_rents(dataset,buildings,year,rent=0,convert_res_rent_to_yearly=0,convert_res_sales_to_yearly=0):
+  buildings['nonres_rent'] = dataset.load_attr('nonresidential_rent',year)
+  if not rent: 
+    buildings['res_rent'] = dataset.load_attr('residential_sales_price',year)
+    if convert_res_sales_to_yearly: buildings.res_rent = np.pmt(developer.INTERESTRATE,developer.PERIODS,buildings.res_rent)*-1
+  else: buildings['res_rent'] = dataset.load_attr('residential_rent',year)*(12 if convert_res_rent_to_yearly else 1)
+  buildings['rent'] = buildings['nonres_rent'].combine_first(buildings['res_rent'])
+  return buildings
+
+def compute_access_variable(dataset,df,varname,agg=1,decay=2):
+  assert dataset.networks.pya # need to generate pyaccess first
+  pya = dataset.networks.pya
+  pya.initializeAccVars(1)
+  nodeidflds = ['_node_id%d'%i for i in range(pya.numgraphs)]
+  nodes = np.transpose(np.asarray(df[nodeidflds],"int32"))
+  num, dist = 0, 15.0
+  pya.initializeAccVar(num,nodes,np.asarray(df[varname],dtype='float32'),preaggregate=0) 
+  return pya.getAllWeightedAverages(num,localradius=15,regionalradius=30,minimumobservations=10,agg=agg,decay=decay) 
+
+def compute_pems_variables(dataset,pya=None,year=None,impedancebasenumber=1):
+
+  print "Computing accessibility variables"
+
+  if pya is None: pya = dataset.networks.pya
+
+  pya.initializeAccVars(4)
+  nodeidflds = ['_node_id%d'%i for i in range(pya.numgraphs)]
+  fullhouseholds = pd.merge(dataset.fetch('households'),dataset.fetch('buildings',addnodeid=1,pya=pya)[nodeidflds],
+                                left_on='building_id',right_index=True,how='left')
+  households = fullhouseholds[fullhouseholds['income'] > 0]
+  households = households.dropna(subset=nodeidflds)
+  households = households.dropna(subset=['income'])
+  nodes = np.transpose(np.asarray(households[nodeidflds],"int32"))
+
+  num, dist = 0, 15.0
+  pya.initializeAccVar(num,nodes,np.asarray(households['income'],dtype='float32'),preaggregate=0) 
+  averageincome = pya.getAllWeightedAverages(num,localradius=15,regionalradius=30,minimumobservations=10) 
+  averageincome[np.where(averageincome<30000)] = 30000
+  averageincome[np.where(averageincome>180000)] = 180000
+
+  households = fullhouseholds[fullhouseholds['tenure']==1] 
+  households = households.dropna(subset=nodeidflds)
+  nodes = np.transpose(np.asarray(households[nodeidflds],dtype="int32"))
+
+  num, dist = 3, 10.0
+  pya.initializeAccVar(num,nodes,np.asarray(households['persons'].fillna(1),dtype='float32')) 
+  renters = pya.getAllAggregateAccessibilityVariables(dist,num,pya.AGG_SUM,pya.DECAY_LINEAR)
+
+  firms = pd.merge(dataset.fetch('nets',addnodeid=1,addbuildingid=1,pya=pya),
+                                dataset.fetch('buildings',addnodeid=1,pya=pya)[nodeidflds],
+                                left_on='building_id',right_index=True,how='left')
+  firms = firms.dropna(subset=nodeidflds)
+  nodes = np.transpose(np.asarray(firms[nodeidflds],dtype="int32"))
+
+  num, dist = 1, 30
+  pya.initializeAccVar(num,nodes,np.asarray(firms['emp11'].fillna(1),dtype='float32'))
+
+  accessibility = pya.getAllAggregateAccessibilityVariables(dist,num,pya.AGG_SUM,pya.DECAY_LINEAR,gno=1,
+                                                                                 impno=impedancebasenumber)
+  accessibility = pya.convertgraphs(1,0,accessibility)
+  reliability =   pya.getAllAggregateAccessibilityVariables(dist,num,pya.AGG_SUM,pya.DECAY_LINEAR,gno=1,
+                                                                                 impno=impedancebasenumber+1)
+  reliability = pya.convertgraphs(1,0,reliability)
+  reliability = accessibility-reliability
+
+  xys = np.transpose(pya.getgraphxys())
+
+  d = {'demo_averageincome_average_local': averageincome,
+       'nets_all_regional1_30':            accessibility,
+       'nets_all_regional2_30':            reliability,
+       'hoodrenters':            		   renters,
+       'x':                                xys[0],
+       'y':                                xys[1]}
+
+  df = pd.DataFrame(d,index=pya.getGraphIDS())
+  print df.describe()
+  return df
