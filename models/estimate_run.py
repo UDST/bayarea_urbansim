71d055e48c77ac3803b10983b123a8cb90557929
Fletcher Foti <ffoti@berkeley.edu>
2014-02-13 11:01:50 -0800

    can now simulate multiple years

--- models/estimate_run.py
+++ models/estimate_run.py
@@ -2,44 +2,47 @@ import os, sys
 import simplejson
 from synthicity.utils import misc
 
+
 sys.path.insert(0,".")
 import dataset
 dset = dataset.BayAreaDataset(os.path.join(misc.data_dir(),'bayarea.h5'))
 
-print "Running rsh_estimate"
-import rsh_estimate
-retval = rsh_estimate.rsh_estimate(dset)
-if retval: open(os.path.join(misc.output_dir(),"rsh_estimate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
-print "Running rsh_simulate"
-import rsh_simulate
-retval = rsh_simulate.rsh_simulate(dset)
-if retval: open(os.path.join(misc.output_dir(),"rsh_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
-print "Running hlcms_estimate"
-import hlcms_estimate
-retval = hlcms_estimate.hlcms_estimate(dset)
-if retval: open(os.path.join(misc.output_dir(),"hlcms_estimate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
-print "Running rrh_estimate"
-import rrh_estimate
-retval = rrh_estimate.rrh_estimate(dset)
-if retval: open(os.path.join(misc.output_dir(),"rrh_estimate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
-print "Running rrh_simulate"
-import rrh_simulate
-retval = rrh_simulate.rrh_simulate(dset)
-if retval: open(os.path.join(misc.output_dir(),"rrh_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
-print "Running hlcmr_estimate"
-import hlcmr_estimate
-retval = hlcmr_estimate.hlcmr_estimate(dset)
-if retval: open(os.path.join(misc.output_dir(),"hlcmr_estimate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
-print "Running nrh_estimate"
-import nrh_estimate
-retval = nrh_estimate.nrh_estimate(dset)
-if retval: open(os.path.join(misc.output_dir(),"nrh_estimate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
-print "Running nrh_simulate"
-import nrh_simulate
-retval = nrh_simulate.nrh_simulate(dset)
-if retval: open(os.path.join(misc.output_dir(),"nrh_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
-print "Running elcm_estimate"
-import elcm_estimate
-retval = elcm_estimate.elcm_estimate(dset)
-if retval: open(os.path.join(misc.output_dir(),"elcm_estimate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
- 
+for year in range(1): 
+  print "Running rsh_estimate"
+  import rsh_estimate
+  retval = rsh_estimate.rsh_estimate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"rsh_estimate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+  print "Running rsh_simulate"
+  import rsh_simulate
+  retval = rsh_simulate.rsh_simulate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"rsh_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+  print "Running hlcms_estimate"
+  import hlcms_estimate
+  retval = hlcms_estimate.hlcms_estimate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"hlcms_estimate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+  print "Running rrh_estimate"
+  import rrh_estimate
+  retval = rrh_estimate.rrh_estimate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"rrh_estimate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+  print "Running rrh_simulate"
+  import rrh_simulate
+  retval = rrh_simulate.rrh_simulate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"rrh_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+  print "Running hlcmr_estimate"
+  import hlcmr_estimate
+  retval = hlcmr_estimate.hlcmr_estimate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"hlcmr_estimate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+  print "Running nrh_estimate"
+  import nrh_estimate
+  retval = nrh_estimate.nrh_estimate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"nrh_estimate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+  print "Running nrh_simulate"
+  import nrh_simulate
+  retval = nrh_simulate.nrh_simulate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"nrh_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+  print "Running elcm_estimate"
+  import elcm_estimate
+  retval = elcm_estimate.elcm_estimate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"elcm_estimate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+   
+
--- models/simulate_run.py
+++ models/simulate_run.py
@@ -2,32 +2,37 @@ import os, sys
 import simplejson
 from synthicity.utils import misc
 
+num = misc.get_run_number()
+
 sys.path.insert(0,".")
 import dataset
 dset = dataset.BayAreaDataset(os.path.join(misc.data_dir(),'bayarea.h5'))
 
-print "Running rsh_simulate"
-import rsh_simulate
-retval = rsh_simulate.rsh_simulate(dset)
-if retval: open(os.path.join(misc.output_dir(),"rsh_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
-print "Running hlcms_simulate"
-import hlcms_simulate
-retval = hlcms_simulate.hlcms_simulate(dset)
-if retval: open(os.path.join(misc.output_dir(),"hlcms_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
-print "Running rrh_simulate"
-import rrh_simulate
-retval = rrh_simulate.rrh_simulate(dset)
-if retval: open(os.path.join(misc.output_dir(),"rrh_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
-print "Running hlcmr_simulate"
-import hlcmr_simulate
-retval = hlcmr_simulate.hlcmr_simulate(dset)
-if retval: open(os.path.join(misc.output_dir(),"hlcmr_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
-print "Running nrh_simulate"
-import nrh_simulate
-retval = nrh_simulate.nrh_simulate(dset)
-if retval: open(os.path.join(misc.output_dir(),"nrh_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
-print "Running elcm_simulate"
-import elcm_simulate
-retval = elcm_simulate.elcm_simulate(dset)
-if retval: open(os.path.join(misc.output_dir(),"elcm_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
- 
+for year in range(3): 
+  print "Running rsh_simulate"
+  import rsh_simulate
+  retval = rsh_simulate.rsh_simulate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"rsh_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+  print "Running hlcms_simulate"
+  import hlcms_simulate
+  retval = hlcms_simulate.hlcms_simulate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"hlcms_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+  print "Running rrh_simulate"
+  import rrh_simulate
+  retval = rrh_simulate.rrh_simulate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"rrh_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+  print "Running hlcmr_simulate"
+  import hlcmr_simulate
+  retval = hlcmr_simulate.hlcmr_simulate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"hlcmr_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+  print "Running nrh_simulate"
+  import nrh_simulate
+  retval = nrh_simulate.nrh_simulate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"nrh_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+  print "Running elcm_simulate"
+  import elcm_simulate
+  retval = elcm_simulate.elcm_simulate(dset,year=2010+year)
+  if retval: open(os.path.join(misc.output_dir(),"elcm_simulate.json"),"w").write(simplejson.dumps(retval,sort_keys=True,indent=4))
+   
+
+dset.save_output(os.path.join(misc.runs_dir(),'run_%d.h5'%num))
