cf2a1bc217055c4d96884115c91c243a5bf7c314
Fletcher Foti <ffoti@berkeley.edu>
2014-02-12 17:48:57 -0800

    seems to be working well

--- run_json.py
+++
@@ -1,9 +0,0 @@
-import os, json, sys, time
-import dataset, variables
-from synthicity.utils import misc
-
-args = sys.argv[1:]
-
-if __name__ == '__main__':  
-  dset = dataset.BayAreaDataset(os.path.join(misc.data_dir(),'bayarea.h5'))
-  for arg in args: misc.run_model(arg,dset,estimate=True,simulate=True,variables=variables)   
