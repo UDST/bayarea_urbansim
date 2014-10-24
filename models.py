79d2029822cf617fdde54d877bacfeab22890c76
Fletcher Foti <fscottfoti@gmail.com>
2014-10-24 12:30:17 -0700

    one more try, I think it's working now

--- models.py
+++ models.py
@@ -70,7 +70,7 @@ def property_taxes(buildings, parcels_geography, acct_settings, coffer, year):
                                                 metadata={
                                                     "year": year
                                                 })
-    print "Sample rows from property tax accounts:"
+    print "Sample rows from property tax accts:"
     print coffer["prop_tax_acct"].to_frame().head(10)
 
 
