96c3217f6ad7e7990add4ef2b0d3925dbfab0c56
Fletcher Foti <fscottfoti@gmail.com>
2014-11-07 11:13:54 -0800

    sort by amount in account

--- models.py
+++ models.py
@@ -71,9 +71,10 @@ def property_taxes(buildings, parcels_geography, acct_settings, coffer, year):
                                                 metadata={
                                                     "year": year
                                                 })
-                                                
+
     print "Sample rows from property tax accts:"
-    print coffer["prop_tax_acct"].to_frame().head(7)
+    print coffer["prop_tax_acct"].to_frame().\
+        sort(columns=["amount"], ascending=False).head(7)
 
 
 @sim.model("travel_model_output")
