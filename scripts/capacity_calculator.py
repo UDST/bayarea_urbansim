0eea2db27160e97ea3bcfb6b6d16c98e3432fc18
fscottfoti <fscottfoti@gmail.com>
2015-08-28 21:51:07 +0000

    bad bug in capacity calculation

--- scripts/capacity_calculator.py
+++ scripts/capacity_calculator.py
@@ -12,14 +12,14 @@ parcels_geography = orca.get_table("parcels_geography")
 
 df = parcels.to_frame(["geom_id", "total_residential_units", "zoned_du", "zoned_du_underbuild", "zoned_du_underbuild_nodev"])
 
-df = df.query("zoned_du_underbuild_nodev > 0")
-print "Number of parcels with value > 0 = %d" % len(df)
-
 df["juris_name"] = parcels_geography.juris_name
 df["juris_id"] = parcels_geography.jurisdiction
 df = df.set_index("geom_id")
 
-df.to_csv('/var/www/html/scratchpad/bayarea_softsites.csv')
+df_filt = df.query("zoned_du_underbuild_nodev > 0")
+print "Number of parcels with value > 0 = %d" % len(df_filt)
+
+df_filt.to_csv('/var/www/html/scratchpad/bayarea_softsites.csv')
 
 df = df.groupby(["juris_name", "juris_id"]).sum()
 df["total_residential_units"] = df.total_residential_units.fillna(0).astype('int')
