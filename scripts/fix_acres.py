4637c2278ae948384ac297d72ddd99e87203a6d0
Tom Buckley <buckley.tom@gmail.com>
2015-11-16 14:22:11 -0800

    notes on method for incorporating abag acre data
    here we try to get a reasonable ratio of change of acres from base year to
    2040 by filling na values for acres with 1's instead of 0's. (otherwise change was infinite in the 100 cases where the base year acre value was 0). unfortunately, in the tests included below in the script, it becomes clear that this approach is also problematic, because if the base value for acreage in abag's numbers is a multiple of the value that we have, then we end up with that multiple multiplied by the percent change--resulting in one TAZ having more than a million acres of development, for example. as a temporary solution, will probably assume that urbansim's growth in acre's can be approximated by a linear regression, and then treat the base year values as intercepts in that regression.

--- scripts/fix_acres.py
+++ scripts/fix_acres.py
@@ -10,15 +10,25 @@ import numpy as np
 # 1) compute the % change in residential acres using the parcels method (ie acres of parcels with residential building on them in 2040 / same for 2010). 
 # 2) compute the output variable by increasing resacre10_abag by this percentage
 # 3) same for ciacre
-from output_assessment import get_base_year_df 
 from output_assessment import get_outcome_df
 from output_assessment import write_outcome_csv
+
+def get_base_year_df(geography='superdistrict'):
+    geography_id = 'zone_id' if geography == 'taz' else geography
+    df = pd.read_csv('data/run0_{}_summaries_2009.csv'.format(geography),
+                     index_col=geography_id)
+    df = df.fillna(1)
+    return df
 
 base_year_df = get_base_year_df(geography='taz')
 outcome_df = get_outcome_df(540,geography='taz')
 acre_df = pd.read_csv('data/zone_forecast_inputs.csv',
                  index_col='zone_id',
                  usecols=['resacre10_abag','ciacre10_abag','zone_id'])
+
+# same as output_assessment, but fillna with 1, since using
+# ratio to decide change in outcome
+
 
 def scaled_ciacre(base_year_df, outcome_df, acre_df):
     s = base_year_df['CIACRE']
@@ -41,28 +51,62 @@ def scaled_resacre(base_year_df, outcome_df, acre_df):
 ciacre = scaled_ciacre(base_year_df, outcome_df, acre_df)
 resacre = scaled_resacre(base_year_df, outcome_df, acre_df)
 
+print (ciacre-outcome_df['CIACRE']).describe()
+# count       1451.000000
+# mean       -3236.698121
+# std        37579.293901
+# min     -1281870.699553
+# 25%         -496.281697
+# 50%          -68.497138
+# 75%           -1.762445
+# max        20182.306218
+
+# check on the (negative) 1 million acre value
+
+
 #check
 (outcome_df['RESACRE']/ciacre).describe()
-(outcome_df['CIACRE']/ciacre).describe()
+print (outcome_df['CIACRE']-ciacre).describe()
 (outcome_df['RESACRE']/ciacre).value_counts()
-(outcome_df['CIACRE']/ciacre).value_counts()
+print (outcome_df['CIACRE']/ciacre).value_counts()
 
 # compare the base year data
 # some ~100 taz are 0/na 
 # in one or the other datasource
 sr = base_year_df['RESACRE']
 sr2 = acre_df['resacre10_abag']
-si = base_year_df['CIACRE']
-si2 = acre_df['ciacre10_abag']
 (sr/sr2).describe()
-(si/si2).describe()
 ((sr/sr2)==0).value_counts()
 ((sr/sr2)==np.inf).value_counts()
-((si/si2)==0).value_counts()
-((si/si2)==np.inf).value_counts()
 
 #assign
-outcome_df['CIACRE'] = ciacre
-outcome_df['RESACRE'] = resacre
+# outcome_df['CIACRE'] = ciacre
+# outcome_df['RESACRE'] = resacre
 
 write_outcome_csv(outcome_df, 540, 'taz', year=2040)
+
+abgc = acre_df["ciacre10_abag"]
+mtcc = base_year_df["CIACRE"]
+us_outc = outcome_df["CIACRE"]
+
+(abgc == mtcc).value_counts()
+print (mtcc/abgc).describe()
+print ((mtcc/abgc)==0).value_counts()
+print ((mtcc/abgc)==np.inf).value_counts()
+
+
+# mtcc = base_year_df['CIACRE']
+
+# s3 = (us_outc - mtcc) / mtcc
+
+# #this method works fine, except for cases in which mtcc is 0
+# s4 = abgc*(1 + s3)
+
+# #there are ~100 base year taz's with ciacre=0
+# (mtcc==0).value_counts()
+
+# #in the outcome year, these are their values. 
+# us_outc[(mtcc==0)]
+
+# #why fill values here? 
+# s4[s3 == np.inf]=us_outc[s3 == np.inf]
