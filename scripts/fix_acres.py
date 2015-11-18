2a2f1c103ee3e66e74c41a1786bddcef64c56e90
Tom Buckley <buckley.tom@gmail.com>
2015-11-17 16:09:28 -0800

    write commercial.residential taz acre csv with max(totacre) conditional
    in 300 taz's, for commercial and industrial acres, a linear model of change in acres based on urbansim inputs and outputs put the acres for this type over the totalacres for the taz. and in 100 taz's the same was the case for resacre. so here we limit the amount for those variables in each taz to 2x the total acres in the taz.

--- scripts/fix_acres.py
+++ scripts/fix_acres.py
@@ -29,12 +29,19 @@ def scaled_ciacre(base_year_df, outcome_df, acre_df):
     abgc = acre_df["ciacre10_abag"]
     mtcc = base_year_df["CIACRE"]
     us_outc = outcome_df["CIACRE"]
+    totacre = outcome_df["TOTACRE"]
+
+    #replace 0's with 1's, because otherwise infinite values below
+    abgc = abgc.replace(0,1)
+    mtcc = mtcc.replace(0,1)
 
     prcnt_chng = (us_outc - mtcc) / mtcc
     combined_acres = abgc*(1 + prcnt_chng)
 
-    abgr = abgr.replace(0,1)
-    mtcr = mtcr.replace(0,1)
+    overtotal = combined_acres[combined_acres > 2*totacre]-totacre[combined_acres > 2*totacre]
+    overtotal.name = 'amnt_ovr_ttcre'
+
+    combined_acres[combined_acres > 2*totacre] = 2*totacre
 
     # we do the following because some base year acreage numbers
     # are absurdly different and if we model forward on them  
@@ -46,19 +53,26 @@ def scaled_ciacre(base_year_df, outcome_df, acre_df):
 
     # # set 83 TAZ's with negative values after the above to 0
     # combined_acres[combined_acres<0]=0
-    return combined_acres
+    return (combined_acres, overtotal)
 
 def scaled_resacre(base_year_df, outcome_df, acre_df):
     abgr = acre_df["resacre10_abag"]
     mtcr = base_year_df["RESACRE"]
     us_outr = outcome_df["RESACRE"]
-
-    prcnt_chng = (us_outr - mtcr) / mtcr
-    combined_acres = abgr*(1 + prcnt_chng)
+    totacre = outcome_df["TOTACRE"]
 
     #replace 0's with 1's, because otherwise infinite values below
     abgr = abgr.replace(0,1)
     mtcr = mtcr.replace(0,1)
+
+    prcnt_chng = (us_outr - mtcr) / mtcr
+    combined_acres = abgr*(1 + prcnt_chng)
+
+    overtotal = combined_acres[combined_acres > 2*totacre]
+    overtotal.name = 'amnt_ovr_ttcre'
+
+    combined_acres[combined_acres > 2*totacre] = 2*totacre
+
     # we do the following because some base year acreage numbers
     # are absurdly different and if we model forward on them  
     # the results are absurd. see this commit for details:
@@ -70,11 +84,31 @@ def scaled_resacre(base_year_df, outcome_df, acre_df):
     # # set ~300 TAZ's with negative values after the above to 0
     # combined_acres[combined_acres<0]=0
 
-    return combined_acres
+    return (combined_acres, overtotal)
 
-# ciacre = scaled_ciacre(base_year_df, outcome_df, acre_df)
-# resacre = scaled_resacre(base_year_df, outcome_df, acre_df)
+ciacre, c_overtotal = scaled_ciacre(base_year_df, outcome_df, acre_df)
+resacre, r_overtotal = scaled_resacre(base_year_df, outcome_df, acre_df)
 
+
+# print (outcome_df['RESACRE']-resacre).describe()
+# print (outcome_df['CIACRE']-ciacre).describe()
+# assign
+outcome_df['CIACRE'] = ciacre
+outcome_df['RESACRE'] = resacre
+
+def write_outlier_csv(df, run, geography, landtype, year=2040):
+    geography_id = 'zone_id' if geography == 'taz' else geography
+    f = 'runs/run%(run)d_%(geography)s_summaries_%(year)d_acre_fix_outliers_%(landtype)s.csv' \
+         % {"run": run, "year": year, "geography": geography, "landtype": landtype}
+    df = df.fillna(0)
+    df.to_csv(f,header=True)
+
+write_outlier_csv(c_overtotal, 540, 'taz', 'commercial', year=2040)
+write_outlier_csv(r_overtotal, 540, 'taz', 'residential', year=2040)
+# print outcome_df.CIACRE.describe()
+# print outcome_df.RESACRE.describe()
+
+write_outcome_csv(outcome_df, 540, 'taz', year=2040)
 
 def report_outliers_in_base_year_data_differences(acre_df,base_year_df):
     abgr = acre_df["resacre10_abag"]
@@ -113,16 +147,4 @@ def report_outliers_in_base_year_data_differences(acre_df,base_year_df):
     r_outliers.to_csv('r_acre_outliers_prcnt.csv')
     c_outliers.to_csv('c_acre_outliers_prcnt.csv')
 
-
-
-#check
-# print (outcome_df['RESACRE']-resacre).describe()
-# print (outcome_df['CIACRE']-ciacre).describe()
-# (outcome_df['RESACRE']/ciacre).value_counts()
-# (outcome_df['RESACRE']/ciacre).value_counts()
-
-# assign
-# outcome_df['CIACRE'] = ciacre
-# outcome_df['RESACRE'] = resacre
-
-write_outcome_csv(outcome_df, 540, 'taz', year=2040)
+# write_outcome_csv(outcome_df, 540, 'taz', year=2040)
