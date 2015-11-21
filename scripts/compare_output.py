99bc23e76254c5aed61edeb0d920660083c9a93c
Tom Buckley <buckley.tom@gmail.com>
2015-11-20 17:17:40 -0800

    compare_outcomes from the command line

--- scripts/compare_output.py
+++ scripts/compare_output.py
@@ -1,5 +1,11 @@
-from output_csv_utils import *
+from output_csv_utils import compare_outcome_for
+import sys
 
-base_year_df = get_base_year_df()
-compare_outcome_for('totemp', runs=[547, 540])
-compare_outcome_for('tothh', runs=[547, 540])
+# compares outcomes from simulation runs
+# example usage:
+# python scripts/compare_output.py 556 572 611
+
+runs = map(int, sys.argv[1:])
+
+compare_outcome_for('totemp', runs=runs)
+compare_outcome_for('tothh', runs=runs)
