3c252c1b128c76dad21eeef58297374a90505056
Tom Buckley <buckley.tom@gmail.com>
2015-12-14 13:58:05 -0800

    compare outcome for pda's and juris too

--- scripts/compare_output.py
+++ scripts/compare_output.py
@@ -7,7 +7,9 @@ import sys
 
 runs = map(int, sys.argv[1:])
 
-compare_outcome_for('totemp', runs=runs)
-compare_outcome_for('tothh', runs=runs)
-
-compare_outcome_for('tothh', geography='taz', runs=runs)
+compare_outcome_for('tothh', set_geography='pda', runs=runs)
+compare_outcome_for('totemp', set_geography='pda', runs=runs)
+compare_outcome_for('tothh', set_geography='juris', runs=runs)
+compare_outcome_for('totemp', set_geography='juris', runs=runs)
+compare_outcome_for('tothh', set_geography='superdistrict', runs=runs)
+compare_outcome_for('totemp', set_geography='superdistrict', runs=runs)
