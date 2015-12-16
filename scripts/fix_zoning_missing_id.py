3742ad2a2511f220951f20da770d55ace516429c
Tom Buckley <buckley.tom@gmail.com>
2015-12-16 10:41:51 -0800

    fix pandas index assignment for zoning_id
    was assigning the wrong index as a new zoning_id. this fixes it for berkeley.'

--- scripts/fix_zoning_missing_id.py
+++ scripts/fix_zoning_missing_id.py
@@ -15,16 +15,17 @@ print "there are " + str(len(null_df.index)) + " empty zoning ids"
 print "number of parcels with null values by city:"
 print null_df.tablename.value_counts()
 
-zldf = zl.to_frame()
+zl_df = zl.to_frame()
 
 zlcn = orca.get_table("zoning_table_city_lookup")
 zlcndf = zlcn.to_frame()
-zldf_tbl_nm = pd.merge(zldf,zlcndf,how='left',left_on='city',right_on='city_name')
+zl_df['zoning_lookup_table_id'] = zl_df.index
+#zlcndf['zoning_lookup_table_id'] = zlcndf.index
+zldf_tbl_nm = pd.merge(zl_df,zlcndf,how='left',left_on='city',right_on='city_name')
 zl_df = zldf_tbl_nm
 
-zl_df['zoning_lookup_table_id'] = zl_df.index
 null_df['geom_id'] = null_df.index
-mdf = pd.merge(null_df,zldf_tbl_nm,how='inner', right_on=['name','tablename'], left_on=['zoning','tablename'])
+mdf = pd.merge(null_df,zl_df,how='inner', right_on=['name','tablename'], left_on=['zoning','tablename'])
 mdf = mdf.set_index(mdf.geom_id)
 
 print "replaced " + str(len(mdf.index)) + " empty zoning ids"
