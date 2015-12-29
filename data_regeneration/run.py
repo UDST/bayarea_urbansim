801a1ab8a5c861bde5d9eb29a5b7ddaf63ab0c90
Tom Buckley <buckley.tom@gmail.com>
2015-12-29 00:34:52 -0500

    add a bunch of comments to data regeneration
    this should make it easier to comprehend what is going on when data is regenerated, and where it is coming from. in addition, the aka's here link it to the makefile in the branch makefile currently in the zoning repo that describes all the data sources for every step of data regeneration, including intersection with admin stuff and zoning id's

--- data_regeneration/run.py
+++ data_regeneration/run.py
@@ -26,12 +26,15 @@ def check_run(filename):
 print("PREPROCESSING: Loading shapefiles by county.")
 
 # Load shapefile data inputs, fix invalid geometries, and reproject.
+# aka load_county_parcel_data_to_staging
 check_run('load.py')
 
 
 print("PROCESSING: Loading parcel attributes by county.")
 
 # Run county attribute processing scripts.
+# aka homogenize_and_clean_county_parcel_attributes
+# a lot of these scripts drop duplicate APN parcels
 county_names = ['ala', 'cnc', 'mar', 'nap', 'scl', 'sfr', 'smt', 'sol', 'son']
 for name in county_names:
     filename = os.path.join('counties', name + '.py')
@@ -41,6 +44,7 @@ for name in county_names:
 print("PROCESSING: Combining to create regional parcels table.")
 
 # Join the county attributes and geometries and union the counties together.
+# aka merge_county_parcel_tables_into_one_spatial_table
 loader = TableLoader()
 sql_path = os.path.join(root_path, 'join_counties.sql')
 with open(sql_path) as sql:
@@ -51,49 +55,59 @@ with open(sql_path) as sql:
 print("PROCESSING: Aggregating condos and stacked parcels.")
         
 # Aggregate parcels for multi-geometry representation of buildings
+# aka spatially_combine_those_parcels
+# this attempts to combine parcel geometries in ways
+# that might relate to the way a developer would think about them
+# filling in common spaces around buildings, combining condos together
 check_run('geom_aggregation.py')
 
 
-print("PROCESSING: Imputing attributes based on POINT data sources.")
+print("PROCESSING: Imputing attributed based on POINT data sources.")
         
 # Applying information based on Redfin, Costar, employment points etc.
+# aka assign_historic_sales_data_to_parcels_by_spatial_proximity
+# assigns redfin and costar data to parcels
+# this also does various checks for the reasonableness of parcel attributes
+# (for example, year built within a reasonable year, price reasonable, etc)
+# and then it also seems to make some decisions about when to assign
+# the redfin sale price to a parcel
 check_run('point_based_imputation.py')
 
 
 print("POSTPROCESSING: Applying spatial operations: tag with location identifiers.")
 
 # Apply spatial operations to append location identifiers.
+# aka tag parcels with administrative names (e.g. TAZ number)
 check_run('spatialops.py')
 
 
 print("PROCESSING: Synthesizing/scaling to match aggregate zonal totals.")
 
 # Matching aggregate zonal totals
+# aka move_jobs_and_houses_to_match_aggregate_taz_2010_totals_csv
 check_run('match_aggregate.py')
 
 
 print("PROCESSING: Adding/populating price fields to the building table.")
 
 # Imputing base-year residential and non-residential prices
+# aka predict_prices_of_buildings_and_parcels_with_regressions
 check_run('price_imputation.py')
 
 
 print("PROCESSING: Allocating households and jobs to buildings.")
 
 # Allocating households and jobs to buildings
+# aka put_people_jobs_on_parcels
 check_run('demand_agent_allocation.py')
 
 
 print("SUMMARIZING: Generating data summaries.")
 
 # Output summary CSV files by county and TAZ.
+# aka summarize_by_taz_and_county
 check_run('summaries.py')
 
-
-# print("SUMMARIZING: Exporting to HDF5 and UrbanCanvas db.")
-
 # Output core tables to HDF5 for UrbanSim.
+# aka export_database_to_h5
 check_run('export_to_h5.py')
-
-# # Output buildings, parcels tables to UrbanCanvas db.
-# check_run('export_to_uc.py')
