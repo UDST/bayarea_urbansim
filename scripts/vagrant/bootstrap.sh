ecebb72044db8a19810d4e30157329efcf01a3e2
Tom Buckley <buckley.tom@gmail.com>
2016-03-01 11:10:28 -0800

    fix path

--- scripts/vagrant/bootstrap.sh
+++ scripts/vagrant/bootstrap.sh
@@ -16,7 +16,7 @@ if [ -f "$file" ]
     bash /bayarea_urbansim/lib/Anaconda-2.3.0-Linux-x86_64.sh -b -p $HOME/anaconda
 else
     wget -P /bayarea_urbansim/lib/ https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
-    bash /vagrant/lib/Anaconda-2.3.0-Linux-x86_64.sh -b -p $HOME/anaconda
+    bash /bayarea_urbansim/lib/Anaconda-2.3.0-Linux-x86_64.sh -b -p $HOME/anaconda
 fi
 
 #put anaconda python in path
