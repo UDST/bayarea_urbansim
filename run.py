a944a6288f85d26069015351efbb067b82ca3228
Sam Maurer <smmaurer@gmail.com>
2016-06-20 18:31:19 -0700

    All model steps working

--- run.py
+++ run.py
@@ -151,7 +151,7 @@ def run_models(MODE, SCENARIO):
             "ual_initialize_residential_units",
             "ual_match_households_to_units",
             "ual_assign_tenure_to_units",
-            #"ual_load_rental_listings",		# required to estimate rental hedonic
+#          	"ual_load_rental_listings",		# required to estimate rental hedonic
         ])
 
         # Estimation steps
@@ -174,7 +174,7 @@ def run_models(MODE, SCENARIO):
             
 			"ual_rsh_simulate",             # residential sales hedonic for units
 			"ual_rrh_simulate",             # residential rental hedonic for units
-# 			"nrh_simulate",                 # non-residential rent hedonic
+			"nrh_simulate",                 # non-residential rent hedonic
 			
 			"ual_assign_tenure_to_new_units",  # (based on higher of predicted price or rent)
  
@@ -182,33 +182,33 @@ def run_models(MODE, SCENARIO):
 			"households_transition",
 			"ual_reconcile_unplaced_households",  # update building/unit/hh correspondence
 
-# 			"jobs_relocation",
-# 			"jobs_transition",
+			"jobs_relocation",
+			"jobs_transition",
 
 			"ual_hlcm_owner_simulate",  	 # allocate owners to vacant owner-occupied units
 			"ual_hlcm_renter_simulate",      # allocate renters to vacant rental units
 			"ual_reconcile_placed_households",  # update building/unit/hh correspondence
 
-# 			"ual_update_building_residential_price",  # apply unit prices to buildings			
-# 			"price_vars",
-# 			"scheduled_development_events",
-# 			"alt_feasibility",
+			"ual_update_building_residential_price",  # apply unit prices to buildings			
+			"price_vars",
+			"scheduled_development_events",
+			"alt_feasibility",
 			
-# 			"residential_developer",
-# 			"developer_reprocess",
-# 			"retail_developer",
-# 			"office_developer",
+			"residential_developer",
+			"developer_reprocess",
+			"retail_developer",
+			"office_developer",
 			
-# 			"ual_remove_old_units",		     # (for buildings that were removed)
-# 			"ual_initialize_new_units",		 # set up units for new residential buildings
-# 			"ual_reconcile_unplaced_households",  # update building/unit/hh correspondence
+			"ual_remove_old_units",		     # (for buildings that were removed)
+			"ual_initialize_new_units",		 # set up units for new residential buildings
+			"ual_reconcile_unplaced_households",  # update building/unit/hh correspondence
 
 			#"topsheet",
 			#"diagnostic_output",
 			#"geographic_summary",
 			#"travel_model_output"
 
-        ], iter_vars=[2010])
+        ], iter_vars=[2010,2011])
 
     elif MODE == "estimation":
 
