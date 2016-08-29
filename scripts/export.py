8e29683204d56d117c7d0c87031fac81aa54fa0d
Fletcher Foti <fscottfoti@gmail.com>
2016-08-29 23:50:33 +0100

    minimize the fields that get exported
    (to reduce the size of the csv)

--- scripts/export.py
+++ scripts/export.py
@@ -5,8 +5,9 @@ import baus.models
 import pandas as pd
 import numpy as np
 
-orca.add_injectable("scenario", "4")
+orca.add_injectable("scenario", "1")
 
+'''
 orca.run([
     "neighborhood_vars",            # local accessibility vars
     "regional_vars",                # regional accessibility vars
@@ -15,19 +16,18 @@ orca.run([
     "nrh_simulate",                 # non-residential rent hedonic
     "price_vars"
 ], iter_vars=[2012])
+'''
 
 fnames = [
     "nodev",
     "manual_nodev",
     "oldest_building_age",
     "sdem",
-    "apn",
     "parcel_id",
-    "geom_id",
     "total_sqft",
+    "first_building_type_id",
     "total_non_residential_sqft",
     "total_residential_units",
-    "total_job_spaces",
     "juris",
     "county",
     "pda",
@@ -36,20 +36,14 @@ fnames = [
     "parcel_size",
     "parcel_acres",
     "oldest_building",
-    "first_building_type_id",
     "general_type",
-    "height",
-    "land_cost",
-    "residential_sales_price_sqft",
     "x",
     "y"
 ]
 
 df = orca.get_table("parcels").to_frame(fnames)
 
-df["land_cost"] = df.land_cost.astype("int")
-
-for use in ["retail", "residential", "office", "industrial"]:
+for use in ["retail", "residential"]:
     df[use+"_is_allowed"] = orca.get_injectable("parcel_is_allowed_func")(use)
 
 settings = orca.get_injectable("settings")
@@ -60,12 +54,11 @@ df = df.query(f)
 
 # get building types
 df["building_type"] = \
-    df.first_building_type_id.map(settings["building_type_map2"])
+    df.first_building_type_id.map({v: k for k, v in settings["building_type_map2"].items()})
 
 df["oldest_building"][df.oldest_building > 2200] = np.nan
 
 # after filter can drop a few fields
-#df = df.drop(["nodev", "oldest_building_age", 
-#    "manual_nodev", "first_building_type_id"], axis=1)
+df = df.drop(["first_building_type_id", "sdem", "nodev", "parcel_acres", "oldest_building_age", "manual_nodev"], axis=1).rename(index=str, columns={"total_sqft": "building_sqft", "total_residential_units": "residential_units", "total_non_residential_sqft": "non_residential_sqft", "oldest_building": "year_built"})
 
 df.to_csv("parcels.csv")
