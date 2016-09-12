7f0aae4ce126fc54cb77bc2c51c0ce1dcb75281e
Fletcher Foti <fscottfoti@gmail.com>
2016-09-12 09:42:15 -0700

    first steps to be able to install baus and import from other dirs

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='baus',
+    version='0.1dev',
+    description='Bay Area UrbanSim.',
+    author='Metropolitan Transportation Commission',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: BSD License'
+    ],
+    packages=find_packages(exclude=['*.tests'])
+)
