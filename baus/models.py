d9bf13f22d69e2b0b3c128d68e208127852043fe
Michael Reilly <rass.mkr@gmail.com>
2016-09-30 11:41:29 -0700

    ch vac

--- baus/models.py
+++ baus/models.py
@@ -1000,11 +1000,11 @@ def correct_baseyear_data(buildings, parcels, jobs):
     # this is the maximum vacancy you can have any a building so it NOT the
     # same thing as setting the vacancy for the entire county
     SURPLUS_VACANCY_COUNTY = buildings_county.map({
-       "Alameda": .65,
+       "Alameda": .60,
        "Contra Costa": .61,
        "Marin": .3,
        "Napa": .7,
-       "San Francisco": .45,
+       "San Francisco": .38,
        "San Mateo": .4,
        "Santa Clara": .32,
        "Solano": .53,
