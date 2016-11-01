ca5e697909364b6aacc968c0a77941786c233050
Ubuntu <fscottfoti@gmail.com>
2016-11-01 18:08:09 +0000

    add out year to argument list

--- scripts/check_rhna.py
+++ scripts/check_rhna.py
@@ -9,11 +9,11 @@ if len(args) == 0:
     sys.exit(0)
 
 runnum = int(args[0])
+out_year = int(args[1])
 
 controls_df = pd.read_csv("data/rhna_by_juris.csv", index_col="juris")
 
-base_year, out_year = 2010, 2040
-juris_df = pd.read_csv("runs/run{}_juris_summaries_{}.csv".format(runnum, base_year),
+juris_df = pd.read_csv("runs/run{}_juris_summaries_{}.csv".format(runnum, 2010),
                        index_col="juris")
 juris_df2 = pd.read_csv("runs/run{}_juris_summaries_{}.csv".format(runnum, out_year),
                         index_col="juris")
