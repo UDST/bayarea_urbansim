681119d8005f9b6e06dff42b200d9f2a1df32f84
Ubuntu <fscottfoti@gmail.com>
2017-03-30 20:37:02 +0000

    pep8

--- baus/summaries.py
+++ baus/summaries.py
@@ -105,7 +105,8 @@ def topsheet(households, jobs, buildings, parcels, zones, year,
     write("Number of residential units in buildings table = %d" % du)
     write("Residential vacancy rate = %.2f" % (1-0 - float(nhh)/du))
 
-    write("Number of residential units in units table = %d" % len(residential_units))
+    write("Number of residential units in units table = %d"
+          % len(residential_units))
     rent_own = residential_units.hownrent.value_counts()
     write("Split of units by rent/own = %s" % str(rent_own))
 
