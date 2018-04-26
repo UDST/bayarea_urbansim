61a0809697cb2672426c48562466d0b96eb6e167
Michael Reilly <rass.mkr@gmail.com>
2018-04-26 14:48:27 -0700

    set up skeleton for SLR inundation table

--- baus/datasources.py
+++ baus/datasources.py
@@ -475,6 +475,12 @@ def zones(store):
     return store['zones'].sort_index()
 
 
+# SLR inundation levels for parcels
+# @orca.table()
+#    def slr_parcel_elevation():
+#    return po.read_csv(data/slr_parcel_elevation.csv index_col=parcel_id)
+
+
 # this specifies the relationships between tables
 orca.broadcast('buildings', 'residential_units', cast_index=True,
                onto_on='building_id')
