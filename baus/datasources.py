55118a31df60c87fb517cbac75209178ed0a7a8c
Max Gardner <gardner.max@gmail.com>
2019-03-26 00:04:54 -0700

    got nrh estimation to work

--- baus/datasources.py
+++ baus/datasources.py
@@ -881,12 +881,12 @@ orca.broadcast('parcels', 'buildings', cast_index=True,
                onto_on='parcel_id')
 # not defined in urbansim_Defaults
 orca.broadcast('tmnodes', 'buildings', cast_index=True, onto_on='tmnode_id')
-orca.broadcast('taz_geography', 'parcels', cast_index=True,
-               onto_on='zone_id')
+orca.broadcast('taz_geography', 'parcels', cast_index=True, onto_on='zone_id')
 orca.broadcast('parcels', 'homesales', cast_index=True, onto_on='parcel_id')
 orca.broadcast('nodes', 'homesales', cast_index=True, onto_on='node_id')
 orca.broadcast('tmnodes', 'homesales', cast_index=True, onto_on='tmnode_id')
 orca.broadcast('nodes', 'costar', cast_index=True, onto_on='node_id')
+orca.broadcast('buildings', 'costar', cast_index=True, onto_on='building_id')
 orca.broadcast('tmnodes', 'costar', cast_index=True, onto_on='tmnode_id')
 orca.broadcast('logsums', 'homesales', cast_index=True, onto_on='zone_id')
 orca.broadcast('logsums', 'costar', cast_index=True, onto_on='zone_id')
