927626cf06558ef7f727056381724908940a56dd
Yuqi <ywang@bayareametro.gov>
2020-05-12 11:14:10 -0700

    add blueprint IZ settings in the output

--- baus/summaries.py
+++ baus/summaries.py
@@ -200,6 +200,10 @@ def config(policy, inputs, run_number, scenario, parcels,
         for item in s[fr1]:
             write("Inclusionary rates are FR1: %d cities are set to %.2f" %
                   (len(item["values"]), item["amount"]))
+    elif scenario in policy["inclusionary_blp_enable"]:
+    	for item in s[scenario]:
+    		write("Inclusionary rates for %d pba50zoningmodcat are set to %.2f" %
+                  (len(item["values"]), item["amount"]))
     elif scenario in s.keys():
         for item in s[scenario]:
             write("Inclusionary rates for %d cities are set to %.2f" %
