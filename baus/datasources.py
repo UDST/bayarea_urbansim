97f973550aad5aa3893243cb09ba052a8c784b9b
Yuqi <ywang@bayareametro.gov>
2020-06-02 13:10:20 -0700

    test fix key error

--- baus/datasources.py
+++ baus/datasources.py
@@ -116,7 +116,7 @@ def inclusionary_housing_settings(policy, scenario):
 
     s = policy['inclusionary_housing_settings']
 
-    if (scenario in ["11", "12", "15"]) and\
+    if (scenario in ["foo", "11", "12", "15"]) and\
        (scenario not in policy["inclusionary_fr2_enable"]):
         print("Using Futures Round 1 (PBA40) inclusionary settings")
         fr1 = str(int(scenario) - 10)
