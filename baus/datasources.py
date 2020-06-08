cb2ea64b7c48824ec4de3c925a5f0dda59af31b6
Michael Reilly <mreilly@bayareametro.gov>
2020-06-08 13:47:23 -0700

    update dev proj name to new file name

--- baus/datasources.py
+++ baus/datasources.py
@@ -624,7 +624,7 @@ def reprocess_dev_projects(df):
 # shared between demolish and build tables below
 def get_dev_projects_table(scenario, parcels):
     df = pd.read_csv(os.path.join(misc.data_dir(),
-                     "2020_05_28_development_projects.csv"))
+                     "2020_0605_1127_development_projects.csv"))
     df = reprocess_dev_projects(df)
 
     # this filters project by scenario
