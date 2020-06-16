0c9fda611a9bed44fc8630f477e5b8620b167721
Yuqi <ywang@bayareametro.gov>
2020-06-15 19:24:26 -0700

    add "calculate_vmt_fees" function to run list

--- baus.py
+++ baus.py
@@ -167,6 +167,7 @@ def get_simulation_models(SCENARIO):
         "retail_developer",
         "office_developer",
         "accessory_units",
+        "calculate_vmt_fees",
 
         # (for buildings that were removed)
         "remove_old_units",
@@ -224,7 +225,8 @@ def get_simulation_models(SCENARIO):
     # calculate VMT taxes
     vmt_settings = \
         orca.get_injectable("policy")["acct_settings"]["vmt_settings"]
-    if SCENARIO in vmt_settings["com_for_com_scenarios"]:
+    if SCENARIO in vmt_settings["com_for_com_scenarios"] and \
+            SCENARIO not in ["21", "22", "23"]:
         models.insert(models.index("office_developer"),
                       "subsidized_office_developer")
 
