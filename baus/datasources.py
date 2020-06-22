089c1abad72559b7bec16707c60c236c6b47a935
Lisa Zorn <lzorn@bayareametro.gov>
2020-06-22 11:23:15 -0700

    Update for updated hybrid zoning input

--- baus/datasources.py
+++ baus/datasources.py
@@ -290,7 +290,7 @@ def costar(store, parcels):
 
 @orca.table(cache=True)
 def zoning_lookup():
-    return pd.read_csv(os.path.join(misc.data_dir(), "2020_06_15_zoning_lookup_hybrid_pba50.csv"),
+    return pd.read_csv(os.path.join(misc.data_dir(), "2020_06_22_zoning_lookup_hybrid_pba50.csv"),
                        index_col='id')
 
 
@@ -298,7 +298,7 @@ def zoning_lookup():
 @orca.table(cache=True)
 def zoning_baseline(parcels, zoning_lookup, settings):
     df = pd.read_csv(os.path.join(misc.data_dir(),
-                     "2020_06_15_zoning_parcels_hybrid_pba50.csv"),
+                     "2020_06_22_zoning_parcels_hybrid_pba50.csv"),
                      index_col="geom_id")
     df = pd.merge(df, zoning_lookup.to_frame(),
                   left_on="zoning_id", right_index=True)
