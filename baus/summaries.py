9bf315b632207474913fbcde8673563a05d3a19f
Bobby Lu <bobbylu.shijia@gmail.com>
2020-08-14 16:29:09 -0700

    adding a space between two code blocks

--- baus/summaries.py
+++ baus/summaries.py
@@ -1201,6 +1201,7 @@ def parcel_summary(parcels, buildings, households, jobs,
     if join_col in parcels_geography.to_frame().columns:
         parcel_gg = parcels_geography.to_frame(["parcel_id", join_col, "juris"])
         df = df.merge(parcel_gg, on = 'parcel_id', how = 'left')
+        
     households_df = orca.merge_tables(
         'households',
         [buildings, households],
