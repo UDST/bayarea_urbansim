46c87d5c7d86c59fe7b82f442b0f31b61277e2be
Yuqi <ywang@bayareametro.gov>
2020-09-22 19:46:19 -0700

    udpate data sources

--- baus/datasources.py
+++ baus/datasources.py
@@ -510,7 +510,7 @@ def parcel_rejections():
 @orca.table(cache=True)
 def parcels_geography(parcels, scenario, settings, policy):
     df = pd.read_csv(
-        os.path.join(misc.data_dir(), "2020_07_10_parcels_geography.csv"),
+        os.path.join(misc.data_dir(), "2020_09_21_parcels_geography.csv"),
         index_col="geom_id")
     df = geom_id_to_parcel_id(df, parcels)
 
