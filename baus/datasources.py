3e51f7d8e59fce762e914462c6137cc319292b85
Bobby Lu <bobbylu.shijia@gmail.com>
2020-10-01 11:05:08 -0700

    change to latest version of development projects

--- baus/datasources.py
+++ baus/datasources.py
@@ -656,7 +656,7 @@ def get_dev_projects_table(scenario, parcels):
     # requires the user has MTC's urban_data_internal
     # repository alongside bayarea_urbansim
     urban_data_repo = ("../urban_data_internal/development_projects/")
-    current_dev_proj = ("2020_0731_1607_development_projects.csv")
+    current_dev_proj = ("2020_1001_0932_development_projects.csv")
     orca.add_injectable("dev_proj_file", current_dev_proj)
     df = pd.read_csv(os.path.join(urban_data_repo, current_dev_proj))
     df = reprocess_dev_projects(df)
