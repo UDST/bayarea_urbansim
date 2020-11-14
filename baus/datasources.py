0fabc52d93c68089e7a373be226e9c4efbb9ac2c
Elizabeth Theocharides <etheocharides@bayareametro.gov>
2020-11-13 23:43:00 -0800

    update development projects list 2020_1112_2041
    https://github.com/BayAreaMetro/urban_data_internal/pull/23
    
    update dp: regionwide renovation projects removal + Mark's public land projects
    fix geom_id issue

--- baus/datasources.py
+++ baus/datasources.py
@@ -699,7 +699,7 @@ def get_dev_projects_table(scenario, parcels):
     # requires the user has MTC's urban_data_internal
     # repository alongside bayarea_urbansim
     urban_data_repo = ("../urban_data_internal/development_projects/")
-    file = "2020_1030_1216_development_projects.csv"
+    file = "2020_1112_2041_development_projects.csv"
     print('Version of development_projects: {}'.format(file))
     current_dev_proj = (file)
     orca.add_injectable("dev_proj_file", current_dev_proj)
