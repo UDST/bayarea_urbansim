25fab3e96c3de0d9f80ad615f719603761432025
Elizabeth Theocharides <etheocharides@bayareametro.gov>
2021-02-26 16:25:33 -0800

    Update dev proj
    https://github.com/BayAreaMetro/urban_data_internal/pull/29/commits
    turn of pub and mall_office projects that are GG+Tra for s26

--- baus/datasources.py
+++ baus/datasources.py
@@ -725,7 +725,7 @@ def get_dev_projects_table(scenario, parcels):
     # requires the user has MTC's urban_data_internal
     # repository alongside bayarea_urbansim
     urban_data_repo = ("../urban_data_internal/development_projects/")
-    file = "2021_0121_1602_development_projects.csv"
+    file = "2021_0225_1757_development_projects.csv"
     print('Version of development_projects: {}'.format(file))
     current_dev_proj = (file)
     orca.add_injectable("dev_proj_file", current_dev_proj)
