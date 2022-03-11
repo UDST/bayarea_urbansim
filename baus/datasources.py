3c147b1aecc16f3b6dcb19a53ee5aba198784143
Yuqi <ywang@bayareametro.gov>
2022-03-11 15:36:56 -0800

    sync up with "develop" branch

--- baus/datasources.py
+++ baus/datasources.py
@@ -563,7 +563,7 @@ def parcel_rejections():
 
 
 @orca.table(cache=True)
-def parcels_geography(parcels, scenario, settings):
+def parcels_geography(parcels, scenario, settings, policy):
     file = os.path.join(misc.data_dir(), "2021_02_25_parcels_geography.csv")
     print('Version of parcels_geography: {}'.format(file))
     df = pd.read_csv(file,
