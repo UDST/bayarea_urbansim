5b5aaa6a7fd76d53da75b179fc2b6786f5e6aae3
Elizabeth Theocharides <etheocharides@bayareametro.gov>
2022-11-04 13:04:23 -0700

    make relocation rates scenario-agnostic

--- baus/models.py
+++ baus/models.py
@@ -58,32 +58,9 @@ def employment_relocation_rates():
 # also includes draft blueprint
 @orca.table(cache=True)
 def household_relocation_rates(scenario, policy):
-    if scenario in policy['futures_scenarios']:
-        if scenario in policy['reloc_fr2_enable']:
-            df = pd.read_csv(os.path.join(orca.get_injectable("inputs_dir"),
-                             "household_relocation_rates_fr2.csv"))
-            orca.add_injectable("hh_reloc", 'activated')
-            print("File used is: household_relocation_rates_fr2.csv")
-        else:
-            df = pd.read_csv(os.path.join(orca.get_injectable("inputs_dir"),
-                             "household_relocation_rates_fr_base.csv"))
-            orca.add_injectable("hh_reloc", 'not activated')
-            print("File used is: household_relocation_rates_fr_base.csv")
-    elif scenario in policy['reloc_db_enable']:
-        df = pd.read_csv(os.path.join(orca.get_injectable("inputs_dir"),
-                         "household_relocation_rates_db_var.csv"))
-        orca.add_injectable("hh_reloc", 'activated')
-        print("File used is: household_relocation_rates_db_var.csv")
-    elif scenario in policy['reloc_fb_enable']:
-        df = pd.read_csv(os.path.join(orca.get_injectable("inputs_dir"),
-                         "household_relocation_rates_fb.csv"))
-        orca.add_injectable("hh_reloc", 'activated')
-        print("File used is: household_relocation_rates_fb.csv")
-    else:
-        df = pd.read_csv(os.path.join(orca.get_injectable("inputs_dir"),
-                         "household_relocation_rates_db_base.csv"))
-        orca.add_injectable("hh_reloc", 'not activated')
-        print("File used is: household_relocation_rates_db_base.csv")
+    df = pd.read_csv(os.path.join(orca.get_injectable("inputs_dir"), "household_relocation_rates.csv"))
+    orca.add_injectable("hh_reloc", 'activated')
+    print("File used is: household_relocation_rates_fb.csv")
     return df
 
 
